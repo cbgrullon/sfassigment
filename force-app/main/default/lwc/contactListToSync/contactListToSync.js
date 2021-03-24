4fdf6911c1f576467899d3c597a380dd7014d8e3
Christopher Bradley Grullón <christopher.gru@gmail.com>
2021-03-23 22:57:29 -0400

    COntact sync completed of ad-hoc

--- force-app/main/default/lwc/contactListToSync/contactListToSync.js
+++ force-app/main/default/lwc/contactListToSync/contactListToSync.js
@@ -2,15 +2,25 @@ import { LightningElement,wire,track } from 'lwc';
 import getContacts from '@salesforce/apex/ContactControllerV2.getContacts';
 import FirstNameField from '@salesforce/schema/Contact.FirstName';
 import LastNameField from '@salesforce/schema/Contact.LastName';
+import LastSyncMessageField from '@salesforce/schema/Contact.LastSyncMessage__c';
+import LastSyncDateField from '@salesforce/schema/Contact.LastSyncDate__c';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
-import AccountIndustryField from '@salesforce/schema/Contact.Account.Industry';
+import AccountIndustryField from '@salesforce/schema/Contact.AccountIndustry__c';
 import getFields from '@salesforce/apex/ContactControllerV2.getFields';
+import getUserFieldsSelection from '@salesforce/apex/ContactControllerV2.getUserFieldsSelection';
+import updateFieldSelection from '@salesforce/apex/ContactControllerV2.updateFieldSelection';
+import sendToOtherOrg from '@salesforce/apex/ContactControllerV2.sendToOtherOrg';
 export default class ContactListToSync extends LightningElement {
     columns = [
         {label: 'First Name',fieldName:FirstNameField.fieldApiName, type: 'text'},
         {label: 'Last Name',fieldName:LastNameField.fieldApiName, type: 'text'},
-        {label: 'Account Industry',fieldName:'AccountIndustry', type: 'text'}
+        {label: 'Account Industry',fieldName:AccountIndustryField.fieldApiName, type: 'text'}
     ];
+    syncResultColumns = [
+        {label: 'First Name', fieldName:FirstNameField.fieldApiName,type:'text'},
+        {label: 'Last Name', fieldName:LastNameField.fieldApiName,type:'text'},
+        {label: 'Error Message', fieldName:LastSyncMessageField.fieldApiName,type:'text'}
+    ]
     firstName;
     lastName;
     industry;
@@ -21,7 +31,12 @@ export default class ContactListToSync extends LightningElement {
     contacts = [];
     haveMore = false;
     cantSync = true;
-    showModal = false;
+    showFieldSelectionModal = false;
+    fields = [];
+    maxRowSelection =20;
+    selectedFields= [];
+    syncResult = [];
+    showSyncResult= false;
     connectedCallback(){
         this.loadContacts(true);
         // getFields({}).then(response=>{
@@ -68,24 +83,82 @@ export default class ContactListToSync extends LightningElement {
         }
     }
     handleSelectedRow(event){
-        // let toAppend = [];
-        // for(let i =0;i<event.detail.selectedRows.length;i++){
-        //     const item = event.detail.selectedRows[i];
-        //     console.warn(item)
-        //     let finded = this.selectedContacts.find((contact)=>{
-        //         return contact.Id === item.Id;
-        //     });
-        //     if(finded)
-        //         continue;
-        //     toAppend.push(item);
-        // }
         this.selectedContacts = [...event.detail.selectedRows];
         this.cantSync = this.selectedContacts == 0;
     }
-    handleSyncClick(){
-        this.showModal = true;
+    async handleSyncClick(){
+        this.showFieldSelectionModal = true;
+        try {
+            let fields = await getFields({});
+            let selectedFields = await getUserFieldsSelection({});
+
+            let selectedValues = [];
+            let options = [];
+            Object.keys(fields).forEach(key=>{
+                let finded = selectedFields.find(f=>f.Name.toLowerCase() === key.toLowerCase());
+                if(finded)
+                    selectedValues.push(key);
+                options.push({value:key,label:fields[key]});
+            });
+            this.fields = options;
+            this.selectedFields = selectedValues;
+        } catch (error) {
+            console.error(error);
+        }
     }
-    closeModal(){
-        this.showModal = false;
+    closeFieldSelectionModal(){
+        this.showFieldSelectionModal = false;
+    }
+    handleFieldSelection(event){
+        this.selectedFields = event.detail.value;
+        updateFieldSelection({fields: this.selectedFields})
+        .then(response=>{
+            let event = new ShowToastEvent({
+                title: 'Fields updated in database',
+                variant: 'success'
+            });
+            this.dispatchEvent(event);
+        }).catch(err=>{
+            let event = new ShowToastEvent({
+                title: 'An error has ocurred.',
+                variant: 'error'
+            });
+            this.dispatchEvent(event);
+            console.error(err);
+        });
+    }
+    async handleConfirmSync(){
+        this.showFieldSelectionModal = false;
+        try {
+            let ids = this.selectedContacts.map(sc=> sc.Id);
+            let response = await sendToOtherOrg({contactsId:ids});
+            if(response== null){
+                let event = new ShowToastEvent({
+                    title: 'An error has ocurred.',
+                    variant: 'error'
+                });
+                this.dispatchEvent(event);
+                console.error('Controller returns null');
+                return;
+            }
+            console.log(response)
+            this.syncResult = response;
+            this.showSyncResult = true;
+            let event = new ShowToastEvent({
+                title: 'Contacts synced',
+                variant: 'success'
+            });
+            this.dispatchEvent(event);
+        } catch (error) {
+            let event = new ShowToastEvent({
+                title: 'An error has ocurred.',
+                variant: 'error'
+            });
+            this.dispatchEvent(event);
+            console.error(error);
+        }
+    }
+    closeSyncModal(){
+        this.showSyncResult = false;
     }
 }
